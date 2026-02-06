d7943a2028fd208bd06d274d14ecf584de1796fc
Sjoulk <111968096+Sjoulk@users.noreply.github.com>
2026-02-06 17:48:36 +0100

    UDP Network driver (#19)
    * Basic UDP RX/TX functions. Includes Test Function.
    
    * Testing TDD.
    
    * UDP broadcast prior to testing. Including init from json config.
    
    * UDP broadcast ready for testing.
    
    * UDP hopefully ready for testing.
    
    * Removed config file. Rx & Tx @ same port.
    
    * Cleaned up comments. Self message filter working.
    
    * Fix merge conflict
    
    ---------
    
    Co-authored-by: Mosazghi Yohannes  Tesfazghi <116307580+Mosazghi@users.noreply.github.com>
    Co-authored-by: Mosazghi <fest121@hotmail.com>

--- cmd/elevator/main.go
+++ cmd/elevator/main.go
@@ -1,64 +1,94 @@
 package main
 
 import (
+	//"flag"
 	"flag"
 	"fmt"
+	"time"
 
-	"github.com/Mosazghi/elevator-ttk4145/internal/elevator"
-	eIO "github.com/Mosazghi/elevator-ttk4145/internal/hw"
+	//"github.com/Mosazghi/elevator-ttk4145/internal/elevator"
+	//eIO "github.com/Mosazghi/elevator-ttk4145/internal/hw"
+
+	network "github.com/Mosazghi/elevator-ttk4145/internal/net"
 )
 
-var numFloors = 4
-
+// var numFloors = 4
 func main() {
 	portNum := flag.String("port", "15657", "specify port number")
 	id := flag.Int("id", 1, "specify elevator ID")
 
 	flag.Parse()
 	fmt.Println("ID: ", *id)
+	fmt.Println("portNum: ", *portNum)
 
-	drvButtons := make(chan eIO.ButtonEvent)
-	drvFloors := make(chan int)
-	drvObstr := make(chan bool)
-	drvStop := make(chan bool)
+	// 	drvButtons := make(chan eIO.ButtonEvent)
+	// 	drvFloors := make(chan int)
+	// 	drvObstr := make(chan bool)
+	// 	drvStop := make(chan bool)
 
-	elevIoDriver := eIO.NewElevIoDriver("localhost:"+*portNum, 4)
+	// 	elevIoDriver := eIO.NewElevIoDriver("localhost:"+*portNum, 4)
 
-	go elevIoDriver.PollButtons(drvButtons)
-	go elevIoDriver.PollFloorSensor(drvFloors)
-	go elevIoDriver.PollObstructionSwitch(drvObstr)
-	go elevIoDriver.PollStopButton(drvStop)
+	// 	go elevIoDriver.PollButtons(drvButtons)
+	// 	go elevIoDriver.PollFloorSensor(drvFloors)
+	// 	go elevIoDriver.PollObstructionSwitch(drvObstr)
+	// 	go elevIoDriver.PollStopButton(drvStop)
 
-	initFloor := elevIoDriver.GetFloor()
+	// 	initFloor := elevIoDriver.GetFloor()
 
-	elev := elevator.NewElevState(initFloor, elevIoDriver.ReadInitialButtons(), elevIoDriver)
+	// 	elev := elevator.NewElevState(initFloor, elevIoDriver.ReadInitialButtons(), elevIoDriver)
 
-	if initFloor == -1 {
-		elev.OnInitBetweenFloors()
+	// 	if initFloor == -1 {
+	// 		elev.OnInitBetweenFloors()
+	// 	}
+
+	// Start network
+	txChan, rxChan, errChan, err := network.UDPRunNetwork()
+	if err != nil {
+		fmt.Printf("Failed to start network: %v\n", err)
+		return
 	}
 
-	stateMachine(drvButtons, drvFloors, drvObstr, drvStop, elev)
-}
+	ticker := time.NewTicker(2 * time.Second)
+	defer ticker.Stop()
 
-func stateMachine(drvButtons chan eIO.ButtonEvent, drvFloors chan int, drvObst chan bool, drvStop chan bool, elev *elevator.ElevState) {
-	prevBehavior := elevator.BIdle
-
+	// Handle all channels
 	for {
-
-		if prevBehavior != elev.Behavior {
-			fmt.Printf("State Trans: %v -> %v\n", prevBehavior, elev.Behavior)
-			prevBehavior = elev.Behavior
-		}
-
 		select {
-		case a := <-drvButtons:
-			elev.OnOrderRequest(a)
-		case a := <-drvFloors:
-			elev.OnNewFloorArrival(a)
-		case a := <-drvObst:
-			elev.OnObstructionSignal(a)
-		case a := <-drvStop:
-			elev.OnStopSignal(a)
+		case msg := <-rxChan:
+			fmt.Printf("Received: %s from %s\n", string(msg.Data), msg.Address.String())
+
+		case err := <-errChan:
+			fmt.Printf("Network error: %v\n", err)
+
+		case <-ticker.C:
+			txChan <- network.UDPMessage{Data: []byte("Hello from A")}
+			fmt.Println("Sent broadcast message")
 		}
 	}
+
+	// stateMachine(drvButtons, drvFloors, drvObstr, drvStop, elev)
 }
+
+// func stateMachine(drvButtons chan eIO.ButtonEvent, drvFloors chan int, drvObst chan bool, drvStop chan bool, elev *elevator.ElevState) {
+
+// 	prevBehavior := elevator.BIdle
+
+// 	for {
+
+// 		if prevBehavior != elev.Behavior {
+// 			fmt.Printf("State Trans: %v -> %v\n", prevBehavior, elev.Behavior)
+// 			prevBehavior = elev.Behavior
+// 		}
+
+// 		select {
+// 		case a := <-drvButtons:
+// 			elev.OnOrderRequest(a)
+// 		case a := <-drvFloors:
+// 			elev.OnNewFloorArrival(a)
+// 		case a := <-drvObst:
+// 			elev.OnObstructionSignal(a)
+// 		case a := <-drvStop:
+// 			elev.OnStopSignal(a)
+// 		}
+// 	}
+// }
--- internal/net/udp.go
+++ internal/net/udp.go
@@ -1,1 +1,143 @@
-package net
+package network
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"syscall"
+	"strings"
+)
+
+const (
+	BROADCAST_IP = "255.255.255.255"
+	THE_ONE_PORT = 30000
+)
+
+type UDPMessage struct {
+	Data    []byte
+	Address *net.UDPAddr
+}
+
+var localIP string
+
+func LocalIP() (string, error) {
+	if localIP == "" {
+		conn, err := net.DialTCP("tcp4", nil, &net.TCPAddr{IP: []byte{8, 8, 8, 8}, Port: 53})
+		if err != nil {
+			return "", err
+		}
+		defer conn.Close()
+		localIP = strings.Split(conn.LocalAddr().String(), ":")[0]
+	}
+	return localIP, nil
+}
+
+/* Creates a UDP socket with SO_REUSEADDR, SO_BROADCAST enabled.
+Allows multiple programs to bind to the same port. */
+func UDPCreateSocket() (*net.UDPConn, error) {
+
+	s, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
+	if err != nil {
+		return nil, fmt.Errorf("[UDPCreateSocket] Socket creation failed: %v", err)
+	}
+
+	err = syscall.SetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+	if err != nil {
+		syscall.Close(s)
+		return nil, fmt.Errorf("[UDPCreateSocket] SO_REUSEADDR failed: %v", err)
+	}
+
+	err = syscall.SetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_BROADCAST, 1)
+	if err != nil {
+		syscall.Close(s)
+		return nil, fmt.Errorf("[UDPCreateSocket] SO_BROADCAST failed: %v", err)
+	}
+
+	// Bind to the the common port. addr.Addr = "0.0.0.0" by default
+	var addr syscall.SockaddrInet4
+	addr.Port = THE_ONE_PORT
+
+	err = syscall.Bind(s, &addr)
+	if err != nil {
+		syscall.Close(s)
+		return nil, fmt.Errorf("[UDPCreateSocket] Bind failed: %v", err)
+	}
+
+	// Convert to *net.UDPConn
+	f := os.NewFile(uintptr(s), "")
+	conn, err := net.FileConn(f)
+	f.Close()
+	if err != nil {
+		return nil, fmt.Errorf("[UDPCreateSocket] FileUDPConn failed: %v", err)
+	}
+
+	udpConn, ok := conn.(*net.UDPConn)
+	if !ok {
+		return nil, fmt.Errorf("[UDPCreateSocket] Failed to assert connection as *net.UDPConn")
+	}
+
+	return udpConn, nil
+}
+
+/* Reads continuously from socket and passes the data to a channel */
+func UDPrx(connection *net.UDPConn, receiveChannel chan<- UDPMessage, errorChannel chan<- error) {
+	buffer := make([]byte, 1024)
+
+	localAddrStr, _ := LocalIP()
+	localAddr := net.ParseIP(localAddrStr)
+
+	for {
+		n, remoteAddress, err := connection.ReadFromUDP(buffer)
+		if err != nil {
+			errorChannel <- fmt.Errorf("[ReadFromUDP] Read error:%v", err)
+			continue
+		}
+
+		data := make([]byte, n)
+		copy(data, buffer[:n])
+
+		if remoteAddress.IP.Equal(localAddr) {
+			//fmt.Println("[UDPrx] Echo spotted") // For testing purposes
+			continue
+		}
+
+		receiveChannel <- UDPMessage{
+			Data:    data,
+			Address: remoteAddress,
+		}
+	}
+}
+
+/* Initializes & runs the UDP network. */
+func UDPRunNetwork() (chan<- UDPMessage, <-chan UDPMessage, <-chan error, error) {
+
+
+	rxChan := make(chan UDPMessage, 20)
+	txChan := make(chan UDPMessage, 20)
+	errChan := make(chan error, 10)
+
+
+	conn, err := UDPCreateSocket()
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("Failed to create socket: %v", err)
+	}
+
+	go UDPrx(conn, rxChan, errChan)
+
+	broadcastAddr := &net.UDPAddr{
+		IP:   net.ParseIP(BROADCAST_IP),
+		Port: THE_ONE_PORT,
+	}
+
+	// Broadcast
+	go func() {
+		for msg := range txChan {
+			_, err := conn.WriteToUDP(msg.Data, broadcastAddr)
+			if err != nil {
+				errChan <- fmt.Errorf("Write error: %v", err)
+			}
+		}
+	}()
+
+	return txChan, rxChan, errChan, nil
+}
